0718c4ef291a163ce74c919f6bc0c47721601e57
Juan Esteban Tourn Pizarro <juanestebantournpizarro123@gmail.com>
2025-12-10 19:51:37 -0300

    Se corrigio problema de los productos en el menu

--- src/components/CartMenu.jsx
+++ src/components/CartMenu.jsx
@@ -1,12 +1,20 @@
 import { IoIosClose } from "react-icons/io";
+import { useCounterStore } from "../stores/useCounterStore";
 
 export function CartMenu ({ isShowMenu, setIsShowMenu }) {
+    const menuProducts = useCounterStore((state) => state.menuProducts) 
+
     return (
         <section className="absolute z-100 backdrop-blur-md w-full h-full">
             <div className="flex items-center justify-center h-full">
                 <IoIosClose size={30} onClick={() => (
                     setIsShowMenu(!isShowMenu)
                 )} />
+                <div>
+                    {menuProducts.map((item) => (
+                        <p key={item.id}>${item.price} - {item.title}</p>
+                    ))}
+                </div>
             </div>
         </section>
     )
--- src/stores/useCounterStore.js
+++ src/stores/useCounterStore.js
@@ -1,6 +1,15 @@
-import { create } from "zustand"
+import { create } from 'zustand'
 
-export const useCounterStore = create((set) => ({
-    count: 0,
-    increment: () => set((state) => ({count: state.count + 1}))
+export const useCounterStore = create(set => ({
+  count: 0,
+  menuProducts: [],
+
+  increment: () => set(state => ({ count: state.count + 1 })),
+  postProduct: (product) =>
+  set(state => {
+    const exists = state.menuProducts.some((p) => p.id === product.id)
+    if (exists) return state // No duplicar
+
+    return { menuProducts: [...state.menuProducts, product] }
+  }),
 }))
